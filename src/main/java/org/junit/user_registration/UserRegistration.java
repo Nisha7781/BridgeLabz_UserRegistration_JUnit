608f76fdbdd75e5a4232f915d2e10aa8cc415f1e
Nisha7781 <nishamali0781@gmail.com>
2024-03-22 10:42:59 +0530

    Master Created

---
+++ src/main/java/org/junit/user_registration/UserRegistration.java
@@ -0,0 +1,9 @@
+package org.junit.user_registration;
+
+public class UserRegistration
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to User Registration Problem");
+    }
+}
