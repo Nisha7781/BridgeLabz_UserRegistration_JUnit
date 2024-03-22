339ecf736b2b3606c7dabe045c1f567b4e0e652d
Nisha7781 <nishamali0781@gmail.com>
2024-03-22 10:51:29 +0530

    validation for first name start with capital & have minimum 3 characters

--- src/main/java/org/junit/user_registration/UserRegistration.java
+++ src/main/java/org/junit/user_registration/UserRegistration.java
@@ -1,9 +1,31 @@
 package org.junit.user_registration;
 
-public class UserRegistration
-{
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+
+    public static boolean isValidFirstName(String name)
+    {
+        String regex = "^[A-Z][a-zA-Z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(name);
+        return matcher.matches();
+    }
     public static void main(String[] args)
     {
-        System.out.println("Welcome to User Registration Problem");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your first name: ");
+        String firstName = sc.nextLine();
+
+        if (isValidFirstName(firstName))
+        {
+            System.out.println("Valid First Name");
+        }
+        else
+        {
+            System.out.println("Invalid First Name");
+        }
     }
 }
