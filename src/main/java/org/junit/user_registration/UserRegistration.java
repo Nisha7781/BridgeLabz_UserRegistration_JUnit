c1ef7e2f5dcf94b1ed664413f6c58a9a2bde9437
Nisha7781 <nishamali0781@gmail.com>
2024-03-23 11:16:45 +0530

    Master Created

--- src/main/java/org/junit/user_registration/UserRegistration.java
+++ src/main/java/org/junit/user_registration/UserRegistration.java
@@ -1,45 +1,11 @@
 package org.junit.user_registration;
-import java.util.Scanner;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class UserRegistration
-{
-
-    public static boolean isValidFirstName(String name)
-    {
-        String regex = "^[A-Z][a-zA-Z]{2,}$";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(name);
-        return matcher.matches();
-    }
-    public static boolean isValidLastName(String name)
-    {
-        String regex = "^[A-Z][a-zA-Z]{2,}$";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(name);
-        return matcher.matches();
-    }
+public class UserRegistration {
     public static void main(String[] args)
-    {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter your first name: ");
-        String firstName = sc.nextLine();
-        System.out.print("Enter your last name: ");
-        String lastName = sc.nextLine();
-
-        if (isValidFirstName(firstName))
-        {
-            System.out.println("Valid First Name");
-        } else {
-            System.out.println("Invalid First Name");
-        }
-
-        if (isValidLastName(lastName))
-        {
-            System.out.println("Valid Last Name");
-        } else {
-            System.out.println("Invalid Last Name");
-        }
-    }
+{
+    System.out.println("Welcome to User Registration Problem");
+}
 }
