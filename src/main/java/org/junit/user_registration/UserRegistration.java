800f0a4ad283f29591cb5e654eb9789666b8a8e6
Nisha7781 <nishamali0781@gmail.com>
2024-03-23 11:24:44 +0530

    Added: Lastname validation that is start with capital & minimum 3 letters

--- src/main/java/org/junit/user_registration/UserRegistration.java
+++ src/main/java/org/junit/user_registration/UserRegistration.java
@@ -6,18 +6,38 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 
     String FirstName;
+    String LastName;
 
-    UserRegistration(String FirstName)
+
+
+    UserRegistration(String FirstName, String LastName)
     {
         this.FirstName = FirstName;
+        this.LastName = LastName;
 
     }
+
 
     public String isValidFirstName()
     {
         String regex = "^[A-Z][a-zA-Z]{2,}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(FirstName);
+        if (matcher.matches())
+        {
+            return "HAPPY";
+        }
+        else
+        {
+            return "SAD";
+        }
+    }
+
+    public String isValidLastName()
+    {
+        String regex = "^[A-Z][a-zA-Z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(LastName);
         if (matcher.matches())
         {
             return "HAPPY";
--- src/test/java/org/junit/user_registration/UserRegistrationTest.java
+++ src/test/java/org/junit/user_registration/UserRegistrationTest.java
@@ -3,13 +3,14 @@ package org.junit.user_registration;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class UserRegistrationTest {
+class UserRegistrationTest
+{
 
     //Test for Firstname validity
     @Test
     void checkFirstname_returnHappy()
     {
-        UserRegistration ur = new UserRegistration("Nisha");
+        UserRegistration ur = new UserRegistration("Nisha","Mali");
         String res = ur.isValidFirstName();
         Assertions.assertEquals("HAPPY", res);
     }
@@ -17,7 +18,7 @@ class UserRegistrationTest {
     @Test
     void checkFirstname_returnSad1()
     {
-        UserRegistration ur = new UserRegistration("nisha");
+        UserRegistration ur = new UserRegistration("nisha", "Mali");
         String res = ur.isValidFirstName();
         Assertions.assertEquals("SAD", res);
     }
@@ -25,8 +26,34 @@ class UserRegistrationTest {
     @Test
     void checkFirstname_returnSad2()
     {
-        UserRegistration ur = new UserRegistration("ni");
+        UserRegistration ur = new UserRegistration("ni", "Mali");
         String res = ur.isValidFirstName();
+        Assertions.assertEquals("SAD", res);
+    }
+
+    //Test for Lastname validity
+    @Test
+    void checkLastname_returnHappy()
+    {
+        UserRegistration ur = new UserRegistration("Nisha", "Mali");
+        String res = ur.isValidLastName();
+        Assertions.assertEquals("HAPPY", res);
+    }
+
+    @Test
+    void checkLastname_returnSad1()
+    {
+        UserRegistration ur = new UserRegistration("Nisha", "mali");
+        String res = ur.isValidLastName();
+        Assertions.assertEquals("SAD", res);
+    }
+
+
+    @Test
+    void checkLastname_returnSad2()
+    {
+        UserRegistration ur = new UserRegistration("Nisha", "Ma");
+        String res = ur.isValidLastName();
         Assertions.assertEquals("SAD", res);
     }
 
